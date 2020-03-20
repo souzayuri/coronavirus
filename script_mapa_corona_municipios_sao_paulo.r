81e58e566a538772bafb45746f04ec1957ed5d21
Maurício Vancine <mauricio.vancine@gmail.com>
2020-03-20 11:03:41 -0300

    Update script_mapa_corona_municipios_sao_paulo.r

--- script_mapa_corona_municipios_sao_paulo.r
+++ script_mapa_corona_municipios_sao_paulo.r
@@ -1,7 +1,7 @@
 #' ---
 #' title: coronavirus municipios de sao paulo
 #' author: mauricio vancine
-#' date: 2020-03-19
+#' date: 2020-03-20
 #' ---
 
 # packages
@@ -17,7 +17,6 @@ da
 # preparate data
 da_mun <- da %>% 
   tidyr::separate(city, c("name_muni", "abbrev_state"), sep = "/") %>% 
-  dplyr::filter(abbrev_state == "SP", is.na(totalCases) == FALSE) %>% 
   dplyr::mutate(name_muni = stringr::str_to_title(name_muni))
 da_mun
 
@@ -25,9 +24,12 @@ da_mun
 tibble::glimpse(da_mun)
 
 # munipality data
-mun <- geobr::read_municipality(code_muni = "all", year = 2018)  %>% 
-  dplyr::filter(abbrev_state == "SP")
+mun <- geobr::read_municipality(code_muni = "all", year = 2018)
 mun
+
+# state data
+sta <- geobr::read_state(code_state = "all", year = 2018)
+sta
 
 # join data
 da_mun_spa <- mun %>% 
@@ -40,19 +42,76 @@ da_mun_spa %>%
   dplyr::filter(is.na(totalCases) == FALSE) %>% 
   sf::st_drop_geometry()
 
-# map
-map <- tm_shape(da_mun_spa) +
+# map for sao paulo
+map_sp <- da_mun_spa %>% 
+  dplyr::filter(abbrev_state == "SP") %>% 
+  tm_shape() +
   tm_polygons(border.col = "gray40", col = "totalCases", palette = "Reds", textNA = "Sem registros", 
               title = "Casos", n = 5, style = "jenks") +
   tm_graticules(lines = FALSE) +
   tm_compass(size = 2.5) +
   tm_scale_bar(text.size = .8) +
-  tm_layout(title = "Casos confirmados de \n Corona Vírus \n no Estado de São Paulo \n (18-03-2020)",
+  tm_layout(title = "Casos confirmados de \n Corona Vírus \n no Estado de São Paulo \n (19-03-2020)",
             title.position = c(.67, .85)) +
   tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.2, 0))
-map
+map_sp
 
 # export
-tmap::save_tmap(map, "corona_virus_municipios_sp.png", dpi = 300)
+tmap::save_tmap(map_sp, "corona_virus_municipios_sp.png", dpi = 300)
+
+
+# map for brazil
+map_br_mun <- da_mun_spa %>% 
+  tm_shape() +
+  tm_polygons(border.col = "gray50", col = "totalCases", palette = "Reds", textNA = "Sem registros", 
+              title = "Casos", n = 5, style = "jenks") +
+  tm_shape(sta) +
+  tm_borders(lwd = 1, col = "gray20") +
+  tm_graticules(lines = FALSE) +
+  tm_compass(position = c(0, 0)) +
+  tm_scale_bar(text.size = .8, position = c(0, 0)) +
+  tm_layout(title = "Casos confirmados de \n Corona Vírus no Brasil \n (19-03-2020)",
+            title.position = c(.6, .9),
+            legend.position = c(.15, .1)) +
+  tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.5, 0))
+map_br_mun
+
+# export
+tmap::save_tmap(map_br_mun, "corona_virus_municipios_br.png", dpi = 300)
+
+
+# state map
+# summarise data
+da_sta <- da_mun %>% 
+  dplyr::select(abbrev_state, totalCases) %>% 
+  dplyr::group_by(abbrev_state) %>% 
+  dplyr::summarise(casos = sum(totalCases))
+da_sta
+
+# join data
+da_sta_spa <- sta %>%
+  dplyr::mutate(abbrev_state = as.character(abbrev_state)) %>% 
+  dplyr::left_join(da_sta, by = "abbrev_state")
+da_sta_spa
+
+# confer
+sf::st_drop_geometry(da_sta_spa)
+
+# map for brazil
+map_br_sta <- da_sta_spa %>% 
+  tm_shape() +
+  tm_polygons(border.col = "gray50", col = "casos", palette = "Reds", textNA = "Sem registros", 
+              title = "Casos", n = 5, style = "jenks") +
+  tm_graticules(lines = FALSE) +
+  tm_compass(position = c(0, 0)) +
+  tm_scale_bar(text.size = .8, position = c(0, 0)) +
+  tm_layout(title = "Casos confirmados de \n Corona Vírus no Brasil \n (19-03-2020)",
+            title.position = c(.6, .9),
+            legend.position = c(.15, .1)) +
+  tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.5, 0))
+map_br_sta
+
+# export
+tmap::save_tmap(map_br_sta, "corona_virus_estados_br.png", dpi = 300)
 
 # end ---------------------------------------------------------------------
