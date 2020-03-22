9fc5a777b7bbc201785fce6e6b28e82fb8ff1d14
mauriciovancine <mauricio.vancine@gmail.com>
2020-03-21 23:31:52 -0300

    22-03-2020

--- 00_script_mapa_corona_municipios_estados.r
+++ 00_script_mapa_corona_municipios_estados.r
@@ -44,7 +44,7 @@ map_state_total <- state_geo_cases %>%
 map_state_total
 
 # export
-tmap::save_tmap(map_state_total, "covid19_estados_brasil_total.png", dpi = 300)
+tmap::save_tmap(map_state_total, "covid19_brasil_estados_total.png", dpi = 300)
 
 # state ms
 map_state_ms <- state_geo_cases %>% 
@@ -60,7 +60,7 @@ map_state_ms <- state_geo_cases %>%
 map_state_ms
 
 # export
-tmap::save_tmap(map_state_ms, "covid19_estados_brasil_ministerio_saude.png", dpi = 300)
+tmap::save_tmap(map_state_ms, "covid19_brasil_estados_ministerio_saude.png", dpi = 300)
 
 # state not ms
 map_state_not_ms <- state_geo_cases %>% 
@@ -77,7 +77,7 @@ map_state_not_ms <- state_geo_cases %>%
 map_state_not_ms
 
 # export
-tmap::save_tmap(map_state_not_ms, "covid19_estados_brasil_nao_confirmado_ministerio_saude.png", dpi = 300)
+tmap::save_tmap(map_state_not_ms, "covid19_brasil_estados_nao_confirmado_ministerio_saude.png", dpi = 300)
 
 # state deaths
 map_state_dea <- state_geo_cases %>% 
@@ -93,7 +93,47 @@ map_state_dea <- state_geo_cases %>%
 map_state_dea
 
 # export
-tmap::save_tmap(map_state_dea, "covid19_estados_brasil_mortes.png", dpi = 300)
+tmap::save_tmap(map_state_dea, "covid19_brasil_estados_mortes.png", dpi = 300)
+
+# state in time -----------------------------------------------------------
+# import data
+state_cases_time <- readr::read_csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv") %>% 
+  dplyr::rename(abbrev_state = state) %>% 
+  dplyr::filter(abbrev_state != "TOTAL")
+state_cases_time
+
+# centroids
+state_geo_cen <- sf::st_centroid(state_geo)
+state_geo_cen
+
+# join data
+state_geo_cen_cases_time <- state_geo_cen %>%
+  dplyr::mutate(abbrev_state = as.character(abbrev_state)) %>% 
+  dplyr::left_join(state_cases_time, by = "abbrev_state") %>% 
+  dplyr::mutate(date = as.factor(date)) %>% 
+  tidyr::drop_na(date)
+state_geo_cen_cases_time
+
+# confer
+sf::st_drop_geometry(state_geo_cen_cases_time)
+
+# state map time
+map_state_total_time <- tm_shape(state_geo, bbox = sf::st_bbox(state_geo)) +
+  tm_polygons() +
+  tm_shape(state_geo_cen_cases_time) +
+  tm_symbols(size = "totalCases", scale = 2, title.size = "Casos", alpha = .5, col = "red", border.col = "darkred") +
+  tm_facets(along = "date") +
+  tm_graticules(lines = FALSE) +
+  tm_compass(position = c(.8, .1)) +
+  tm_scale_bar(text.size = .8, position = c(.55, .02)) +
+  tm_layout(title = "Casos confirmados de \n COVID19 no Brasil \n ao longo dos dias",
+            title.position = c(.6, .9),
+            legend.position = c("left", "bottom")) +
+  tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.5, 0))
+
+# export
+tmap::tmap_animation(tm = map_state_total_time, filename = "covid19_brasil_estados_evolucao.gif", wi = 2000, he = 2000, delay = 100)
+# magick::image_read("brasil_states.gif")
 
 # municipality ------------------------------------------------------------
 # import data
@@ -135,7 +175,7 @@ map_br_mun <- mun_geo_cases %>%
 map_br_mun
 
 # export
-tmap::save_tmap(map_br_mun, "covid19_municipios_brasil.png", dpi = 300)
+tmap::save_tmap(map_br_mun, "covid19_brasil_municipios.png", dpi = 300)
 
 # summary and confer data
 mun_geo_cases %>% 
@@ -144,9 +184,10 @@ mun_geo_cases %>%
   dplyr::filter(abbrev_state == "SP")
 
 # map for sao paulo
-map_sp <- mun_geo_cases %>% 
-  dplyr::filter(abbrev_state == "SP") %>% 
-  tm_shape() +
+mun_geo_cases_sp <- mun_geo_cases %>% 
+  dplyr::filter(abbrev_state == "SP")
+
+map_sp <- tm_shape(mun_geo_cases_sp) +
   tm_polygons(border.col = "gray40", col = "totalCases", palette = "Reds", textNA = "Sem registros", 
               title = "Casos", n = 10, style = "pretty") +
   tm_graticules(lines = FALSE) +
@@ -158,34 +199,34 @@ map_sp <- mun_geo_cases %>%
 map_sp
 
 # export
-tmap::save_tmap(map_sp, "covid19_municipios_sao_paulo.png", dpi = 300)
+tmap::save_tmap(map_sp, "covid19_sao_paulo_municipios.png", dpi = 300)
 
-# state in time -----------------------------------------------------------
+# municipality in time ----------------------------------------------------
 # import data
-state_cases_time <- readr::read_csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv") %>% 
-  dplyr::rename(abbrev_state = state) %>% 
-  dplyr::filter(abbrev_state != "TOTAL")
-state_cases_time
+mun_cases_time <- readr::read_csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv") %>% 
+  dplyr::filter(city != "TOTAL") %>% 
+  tidyr::separate(city, c("name_muni", "abbrev_state"), sep = "/") %>% 
+  dplyr::mutate(name_muni = stringr::str_to_title(name_muni))
 
 # centroids
-state_geo_cen <- sf::st_centroid(state_geo)
-state_geo_cen
+mun_geo_cen <- geobr::read_municipal_seat(year = 2010)
+mun_geo_cen
 
 # join data
-state_geo_cen_cases_time <- state_geo_cen %>%
-  dplyr::mutate(abbrev_state = as.character(abbrev_state)) %>% 
-  dplyr::left_join(state_cases_time, by = "abbrev_state") %>% 
+mun_geo_cen_cases_time <- mun_geo_cen %>%
+  dplyr::mutate(name_muni = as.character(name_muni)) %>% 
+  dplyr::left_join(mun_cases_time, by = "name_muni") %>% 
   dplyr::mutate(date = as.factor(date)) %>% 
   tidyr::drop_na(date)
-state_geo_cen_cases_time
+mun_geo_cen_cases_time
 
 # confer
-sf::st_drop_geometry(state_geo_cen_cases_time)
+sf::st_drop_geometry(mun_geo_cen_cases_time)
 
-# state total - demora
-map_state_total_time <- tm_shape(state_geo, bbox = sf::st_bbox(state_geo)) +
+# municipality map time
+map_mun_total_time <- tm_shape(state_geo, bbox = sf::st_bbox(state_geo)) +
   tm_polygons() +
-  tm_shape(state_geo_cen_cases_time) +
+  tm_shape(mun_geo_cen_cases_time) +
   tm_symbols(size = "totalCases", scale = 2, title.size = "Casos", alpha = .5, col = "red", border.col = "darkred") +
   tm_facets(along = "date") +
   tm_graticules(lines = FALSE) +
@@ -197,7 +238,29 @@ map_state_total_time <- tm_shape(state_geo, bbox = sf::st_bbox(state_geo)) +
   tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.5, 0))
 
 # export
-tmap::tmap_animation(tm = map_state_total_time, filename = "brasil_states_time.gif", wi = 2000, he = 2000, delay = 100)
+tmap::tmap_animation(tm = map_mun_total_time, filename = "covid19_brasil_municipios_evolucao.gif", wi = 2000, he = 2000, delay = 100)
 # magick::image_read("brasil_states.gif")
+
+# sao paulo municipality map time
+mun_geo_sp <- mun_geo %>% 
+  dplyr::filter(abbrev_state == "SP")
+
+map_mun_sp_time <- tm_shape(mun_geo_sp, bbox = sf::st_bbox(mun_geo_sp)) +
+  tm_polygons() +
+  tm_shape(mun_geo_cen_cases_time %>% dplyr::filter(abbrev_state.x == "SP")) +
+  tm_symbols(size = "totalCases", scale = 2, title.size = "Casos", alpha = .5, col = "red", border.col = "darkred") +
+  tm_facets(along = "date") +
+  tm_graticules(lines = FALSE) +
+  tm_compass(position = c(.8, .1)) +
+  tm_scale_bar(text.size = .8, position = c(.55, .02)) +
+  tm_layout(title = "Casos confirmados de \n COVID19 em São Paulo \n ao longo dos dias",
+            title.position = c(.6, .9),
+            legend.position = c("left", "bottom")) +
+  tm_credits("Fonte: https://labs.wesleycota.com/sarscov2/br", position = c(.5, 0))
+
+# export
+tmap::tmap_animation(tm = map_mun_sp_time, filename = "covid19_sao_paulo_municipios_evolucao.gif", wi = 2000, he = 2000, delay = 100)
+# magick::image_read("brasil_states.gif")
+
 
 # end ---------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # Coronavírus no Brasil
 
 ## Número de casos total por estado do Brasil
+<img src="covid19_brasil_estados_total.png" align="center" alt="" width="600" />
 
-<img src="covid19_estados_brasil_total.png" align="center" alt="" width="600" />
 
 ## Número de casos confirmados pelo Ministério da Saúde por estado do Brasil
+<img src="covid19_brasil_estados_ministerio_saude.png" align="center" alt="" width="600" />
 
-<img src="covid19_estados_brasil_ministerio_saude.png" align="center" alt="" width="600" />
 
 ## Número de casos não confirmados pelo Ministério da Saúde por estado do Brasil
+<img src="covid19_brasil_estados_nao_confirmado_ministerio_saude.png" align="center" alt="" width="600" />
 
-<img src="covid19_estados_brasil_nao_confirmado_ministerio_saude.png" align="center" alt="" width="600" />
 
 ## Número de mortes por estado do Brasil
+<img src="covid19_brasil_estados_mortes.png" align="center" alt="" width="600" />
 
-<img src="covid19_estados_brasil_mortes.png" align="center" alt="" width="600" />
 
 ## Número de casos por municípios de São Paulo
+<img src="covid19_sao_paulo_municipios.png" align="center" alt="" width="600" />
 
-<img src="covid19_municipios_sao_paulo.png" align="center" alt="" width="600" />
 
 ## Evolução do número de casos por estado
+<img src="covid19_brasil_estados_evolucao.gif" align="center" alt="" width="600" />
 
-<img src="brasil_states_time.gif" align="center" alt="" width="600" />
+
+## Evolução do número de casos por município
+<img src="covid19_brasil_municipios_evolucao.gif" align="center" alt="" width="600" />
+
+
+## Evolução do número de casos por município de São Paulo
+<img src="covid19_sao_paulo_municipios_evolucao.gif" align="center" alt="" width="600" />
